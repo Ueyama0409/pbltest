d3079ec407ce4c4ba23a6be4f6b9db4f11fa0d11
NTE003 <st317a1002r.ueyama@urayama.ac.jp>
2020-06-29 15:06:33 +0900

    テスト

---
+++ app/src/main/java/com/example/pbltest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pbltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //コミットテスト
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
